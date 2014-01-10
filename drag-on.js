9f310840ffe09c8a38aa8f660004ae3ff48a58b0
PretorDH <pretordh@deparadox.com>
2014-01-10 17:47:25 +0200

    New release 2.6.5.
    ! Fixed: crush bugs;
    ! Fixed: best block position calculation for mousewheel events;
    + Add: Send current event to blocks with "no-dragon";
    + Add: Unified parent search;
    + Add: Unified block visibility calculation.

--- drag-on.js
+++ drag-on.js
@@ -1,5 +1,5 @@
 ﻿/**
- * jQuery.Drag-On v2.5.2
+ * jQuery.Drag-On v2.6.5
  * @author Dark Heart aka PretorDH
  * @site dragon.deparadox.com
  * MIT license
@@ -19,164 +19,182 @@ $(function () {
             	exclusion : {'input': '', 'textarea': '', 'select': '', 'object':'' , 'iframe':'' , 'id':'#gmap,#map-canvas'},
             	cursor : 'all-scroll',
             	easing : 'true'
-            },Sd;
+            },_this;
             
             function onPrevent(E) {
                 var e = E || event, et = (e && e.target && (e.target.tagName || e.target.localName || e.target.nodeName).toLowerCase());
-                return e && et && (et in Sd.opt.exclusion || (e.target.href || $(e.target).parents().attr('href') && (e.stopPropagation && e.stopPropagation(), true)))
+                return e && et && (et in _this.opt.exclusion || (e.target.href || $(e.target).parents().attr('href') && (e.stopPropagation && e.stopPropagation(), true)))
 				   || (e && e.preventDefault && e.preventDefault(), e.stopPropagation && e.stopPropagation(), false);
             };
 
-            Sd = (S = $(S)).DragOn = {
+			S = $(S);
+            _this = {
             	opt: (function (opt) {for (var b in opt) def[b]=opt[b]; return def;})(opt),
             	moment: {},
-            	x: 1,
-            	y: 1,
+            	bypass : false,
+            	mx: 1,
+            	my: 1,
             	on:true,
             	toggle: function(e) {
-            		if (Sd.on) {
-			            S.css({ cursor: Sd.opt.cursor }).children('a').on('mousedown',onPrevent).css({ cursor: 'pointer' });
-			            S.on({'mousewheel wheel':Sd.onWhell,'mousedown':Sd.onHold});
-			            $('body').on({'keydown':Sd.onKeyDown,'keyup':Sd.onKeyDown});
+            		if (_this.on) {
+			            S.css({ cursor: _this.opt.cursor }).children('a').on('mousedown',onPrevent).css({ cursor: 'pointer'});
+			            S.on({'mousewheel wheel':_this.onWhell,'mousedown':_this.onHold});
+			            $('body').on({'keydown':_this.onKeyDown,'keyup':_this.onKeyDown});
 			        } else {
 			            S.css({ cursor: '' }).children('a').off('mousedown',onPrevent).css({ cursor: '' });
-			            S.off({'mousewheel wheel':Sd.onWhell,'mousedown':Sd.onHold});
-			            $('body').off({'keydown':Sd.onKeyDown,'keyup':Sd.onKeyDown});
+			            S.off({'mousewheel wheel':_this.onWhell,'mousedown':_this.onHold});
+			            $('body').off({'keydown':_this.onKeyDown,'keyup':_this.onKeyDown});
 			        }
-			        Sd.on=!Sd.on;
-				if (e!=null) S.trigger('BarOn.toggle');
-			        return false;
+			        _this.on=!_this.on;
+			        if (e!=null) S.trigger('BarOn.toggle');
+			        return true;
             	},
                 getCurPos: function () {
-                    var a, b, c, to = S.to;
+                    var b, to = S.to;
                     return S.curPos = {
-                        't': a = to.scrollTop(),
+                        't': to.scrollTop(),
                         'ph': b = to.innerHeight(),
-                        'maxY': b = to[0].scrollHeight - b,
-                        'l': a = to.scrollLeft(),
+                        'maxY': to[0].scrollHeight - b,
+                        'l': to.scrollLeft(),
                         'pw': b = to.innerWidth(),
-                        'maxX': b = to[0].scrollWidth - b
+                        'maxX': to[0].scrollWidth - b
                     }
                 },
                 setCurPos: function (dx, dy) {
-                    var t, l, cp, ddy, ddx;
-                    do {
-                    	S.to = Sd.scrollParent(S.to);
-                        cp = Sd.getCurPos();
+                    var t, l, cp=S.to, ddy, ddx;
+                    
+                    while (S.to=_this.scrollParent(S.to)) {
+                        cp = _this.getCurPos();
                         (cp.maxY > 0) && (Math.abs(dy) > Math.abs(dx))
 							&& ((cp.maxX > 0) || (dx = 0), ddy = (cp.t - (ddy = Math.round((cp.maxY / cp.ph + 1) * dy)) < 0) ? cp.t : (cp.t - ddy > cp.maxY ? cp.t - cp.maxY : ddy))
 							&& S.to.scrollTop(cp.t - ddy), (t = S.to.scrollTop() != cp.t) && (dy = 0,S.to.trigger('scroll'));
                         (cp.maxX > 0) && (Math.abs(dx) > Math.abs(dy))
 							&& (dy = 0, ddx = (cp.l - (ddx = Math.round((cp.maxX / cp.pw + 1) * dx)) < 0) ? cp.l : (cp.l - ddx > cp.maxX ? cp.l - cp.maxX : ddx))
 							&& S.to.scrollLeft(cp.l - ddx), (l = S.to.scrollLeft() != cp.l) && (dx = 0,S.to.trigger('scroll'));
-                    } while ((S.to.data('overflow') == 'no-dragon'
-								|| S.to[0].tagName.toLowerCase() == 'a'
-								|| !(dy && t) && !(dx && l))
-							&& S[0] != S.to[0]
-							&& (S.to = S.to.parent()));
+						if (!(dy && t) && !(dx && l) && S[0]!=S.to[0]) 
+							S.to=S.to.parent() 
+						else return;
+                    };
                 },
-                scrollParent : function(Sto) {
-					while ((Sto[0].nodeType != 1 || Sto.css('overflow')=='visible') || Sto.data('overflow')=='no-dragon' && S[0]!=Sto[0] ) Sto=Sto.parent();
+                scrollParent : function(Sto,w) {
+					while (Sto && Sto[0] && S[0]!=Sto[0] && ((Sto[0].nodeType != 1 || Sto.css('overflow')=='visible') || Sto[0].tagName.toLowerCase() == 'a' || (!w && Sto.data('overflow')=='no-dragon')) ) Sto=Sto.parent();
 					return Sto;
                 },
+				isInBestPosition : function(Sto,Stoo,cp) {
+					var t = Sto.offset(),
+                        p = Stoo.offset(),
+                    	h = Stoo.innerHeight()-cp.ph,
+                        w = Stoo.innerWidth()-cp.pw;
+                        
+                    if (Stoo[0]==document.body && Stoo[0]!==Sto[0]) {
+                    	t.top = t.top -Stoo[0].scrollTop;
+                    	t.left= t.left-Stoo[0].scrollLeft;
+                    };
+                            
+                    return p.top+(h<0?0:h) >= t.top && p.top+(h>0?0:h) <= t.top &&
+                           p.left+(w<0?0:w) >= t.left && p.left+(w>0?0:w) <= t.left;
+				},
+                onWhell: function (e, delta) { 
+                	if (_this.bypass) return (_this.bypass=false,true);
 
-                onWhell: function (e, delta) { //for horizontal scroll
-                    Sd.moment={};
-                    var SSto,cp,st,ot,t,l,ph,pw,et,ad
+                    _this.moment={};
+                    var SSto,cp,st,sl,t,l,ph,pw,et,ad
+                    
                     	E = e.originalEvent;     
-					Sd.x = 1; Sd.y = 1;
+					_this.mx = 1; _this.my = 1;
                     S.to = $((this === e.target) ? this : e.target);
 
-                    delta = (delta || E.wheelDelta || E.wheelDeltaY || E.wheelDeltaX ) >> 1;
-                    delta = delta || (-(E.deltaX || E.deltaY || E.deltaZ)<<(E.deltaMode && E.deltaMode<<2)<<1);
+                    delta = Math.round(delta || (E.wheelDelta || E.wheelDeltaY || E.wheelDeltaX ) >> 1 || (-(E.deltaX || E.deltaY || E.deltaZ)<<(E.deltaMode && E.deltaMode<<2)<<1));
                     ad=Math.abs(delta>>1);
 
                     do {
-	                    S.to = Sd.scrollParent(S.to);
-                        S.too=(SSto = S.to[0]!=S[0])?Sd.scrollParent(S.to.parent()):S;
+	                    S.to = _this.scrollParent(S.to,1);
+                        S.too=(SSto = S.to[0]!=S[0])?_this.scrollParent(S.to.parent(),1):S;
+						cp = _this.getCurPos();
 
-                        cp = Sd.getCurPos();                        
-                		st=S.to.offset();				ot=S.too.offset();
-                		t=st.top-ot.top;				l=st.left-ot.left;
-                		ph=S.too.innerHeight()-cp.ph;	pw=S.too.innerWidth()-cp.pw;
-                    		
-                    	if  ( (ph>=0 && -ad<=t && t<=ph+ad || ph<=0 && ad>=t && t>=ph-ad) && (pw>=0 && -ad<=l && l<=pw+ad || pw<=0 && ad>=l && l>=pw-ad) ){
-	                        if (cp.maxY > 0) {
-                        		S.to.scrollTop(t = (t = cp.t - delta) > 0 ? (t > cp.maxY ? cp.maxY : t) : 0);
-                        		if ( S.to.scrollTop()!=cp.t ) (e.preventDefault(),e.stopPropagation(),S.to.trigger('scroll',[false,false]),delta=0);
+						if (_this.isInBestPosition(S.to,S.too,cp)) {
+							if (S.to.data('overflow') == 'no-dragon') {
+								_this.bypass=true;
+								S.to.trigger(e);
+								return;
+							};
+	                        if ( cp.maxY > 0) {
+	                        	S.to.scrollTop ( (t = cp.t - delta) < 0 ? 0 : (t > cp.maxY ? cp.maxY : t) );
+                        		if ( S.to.scrollTop()!=cp.t) (e.preventDefault(),e.stopPropagation(),S.to.trigger('scroll',[false,false]),delta=0);
                         	};
 	                        if (delta && cp.maxX > 0) {
-                        		S.to.scrollLeft(t = (t = cp.l - delta) > 0 ? (t > cp.maxX ? cp.maxX : t) : 0);
+                        		S.to.scrollLeft( (t = cp.l - delta) > 0 ? (t > cp.maxX ? cp.maxX : t) : 0);
                         		if ( S.to.scrollLeft()!=cp.l ) (e.preventDefault(),e.stopPropagation(),S.to.trigger('scroll',[true,true]),delta=0);
                         	};
                         };
-                        
-                    } while (delta!=0 && SSto && (S.to=S.to.parent()) );
+                    } while (delta && SSto && (S.to=S.to.parent()) );
                     return this;
                 },
                 onHold: function (e) {        
-                    Sd.moment={};
+                    _this.moment={};
                     var b,et = (e.target.tagName || e.target.localName || e.target.nodeName).toLowerCase();
-                    if (et in Sd.opt.exclusion) return;
+                    if (et in _this.opt.exclusion) return;
                   
                     S.too = S.to = $((this === e.target) ? this : e.target);
-                    if (S.too.parents(Sd.opt.exclusion.id).length) return;
+                    if (S.to.parents(_this.opt.exclusion.id).length) return;
                     
                     (e.type == 'mousedown') && (e.preventDefault(), e.stopPropagation());
-                    Sd.x=S.to.hasClass('bBarOn')?-1:1;
-                    Sd.y=S.to.hasClass('rBarOn')?-1:1;
+                    _this.mx=S.to.hasClass('bBarOn')?-1:1;
+                    _this.my=S.to.hasClass('rBarOn')?-1:1;
 					
-					Sd.moment = S.holdPos = { 'x': e.pageX, 'y': e.pageY };
-					Sd.moment.startTime=+new Date();
-                    S.on({'mousemove':Sd.onDragg,'mouseleave mouseup':Sd.onRelease});
-                    S.too.on('mouseup', Sd.onRelease);
+					_this.moment = S.holdPos = { 'x': e.screenX, 'y': e.screenY };
+					_this.moment.startTime=+new Date();
+                    S.on({'mousemove':_this.onDragg,'mouseleave mouseup':_this.onRelease});
+                    S.too.on('mouseup', _this.onRelease);
 
-                    Sd.noButtonHold = false;
-                    (Sd.SAH = S.too).on('scroll', Sd.onScrollAfterHold);
+                    _this.noButtonHold = false;
+                    (_this.SAH = S.too).on('scroll', _this.onScrollAfterHold);
                 },
                 onScrollAfterHold: function (e) {
-                	Sd.moment = {};
-                    Sd.noButtonHold = true;
-                    Sd.SAH.off('scroll', Sd.onScrollAfterHold);
+                	_this.moment = {};
+                    _this.noButtonHold = true;
+                    _this.SAH.off('scroll', _this.onScrollAfterHold);
                 },
                 onDragg: function (e) {
-                    Sd.SAH && (Sd.SAH.off('scroll', Sd.onScrollAfterHold), Sd.SAH = null);
-                    var x = e.pageX, y = e.pageY,
-					dx = x - S.holdPos.x; dy = y - S.holdPos.y;
-                    S.to = $((this === e.target) ? this : e.target);
+                    _this.SAH && (_this.SAH.off('scroll', _this.onScrollAfterHold), _this.SAH = null);
                     
-                    if (Sd.noButtonHold || !(e.which + e.button)) return Sd.onRelease(e);
+                    if (_this.noButtonHold || !(e.which + e.button)) return _this.onRelease(e);
                     e.preventDefault(); e.stopPropagation();
 
+                    var x = e.screenX, 
+                    	y = e.screenY,
+						dx = x - S.holdPos.x, 
+						dy = y - S.holdPos.y;
+                    S.to = $(this===e.target?this:e.target);            
                     S.holdPos = { 'x': x, 'y': y };
-                    Sd.setCurPos(dx*Sd.x, dy*Sd.y);
-                    return true;
+                    
+                    _this.setCurPos(dx*_this.mx, dy*_this.my);
                 },
                 onRelease: function (e) {
-                	var sm;
-                	Sd.opt.easing && (sm=Sd.moment) &&
-                		(sm.vector={y:e.pageY-sm.y,x:e.pageX-sm.x},
+                	var sm; 
+                	_this.opt.easing && (sm=_this.moment) &&
+                		(sm.vector={y:e.screenY-sm.y,x:e.screenX-sm.x},
                 		 sm.snatch=(+new Date()-sm.startTime),
-                		 sm.speedX=((sm.vector.x>0)?1:-1)*sm.vector.x*sm.vector.x/(2*sm.snatch),
-                		 sm.speedY=((sm.vector.y>0)?1:-1)*sm.vector.y*sm.vector.y/(2*sm.snatch),
-                		 (sm.snatch<350)&&(sm.ORE=setTimeout(Sd.onReleaseEasing,10)));
+                		 sm.speedX=((sm.vector.x>0)?1:-1)*sm.vector.x*sm.vector.x/(sm.snatch<<1),
+                		 sm.speedY=((sm.vector.y>0)?1:-1)*sm.vector.y*sm.vector.y/(sm.snatch<<1),
+                		 (sm.snatch<350)&&(sm.ORE=setTimeout(_this.onReleaseEasing,10)));
                 
                     if (e.type in { 'mouseup': '', 'mouseleave': '' }) (e.preventDefault(), e.stopPropagation(), e.stopImmediatePropagation());
-                    Sd.SAH && (Sd.SAH.off('scroll', Sd.onScrollAfterHold), Sd.SAH = null);
-                    S.off({'mouseleave mouseup':Sd.onRelease,'mousemove':Sd.onDragg});
-                    S.too && S.too.off('mouseup', Sd.onRelease);
+                    _this.SAH && (_this.SAH.off('scroll', _this.onScrollAfterHold), _this.SAH = null);
+                    S.off({'mouseleave mouseup':_this.onRelease,'mousemove':_this.onDragg});
+                    S.too && S.too.off('mouseup', _this.onRelease);
                     return true;
                 },
                 onReleaseEasing: function (e) {
                 	var sm;
-                	if (!(sm=Sd.moment)) return;
+                	if (!(sm=_this.moment)) return;
                 	
                     S.to=S.too;
-                    Sd.setCurPos(Sd.x*(sm.speedX*=0.98), Sd.y*(sm.speedY*=0.98));
-                    sm.ORE=(Math.round(sm.speedX)||Math.round(sm.speedY))?setTimeout(Sd.onReleaseEasing,10):null;
+                    _this.setCurPos(_this.mx*(sm.speedX*=0.98), _this.my*(sm.speedY*=0.98));
+                    sm.ORE=(Math.round(sm.speedX)||Math.round(sm.speedY))?setTimeout(_this.onReleaseEasing,10):null;
                 },
                 onKeyDown: function (e) {
-                	var	so,to,too,ek=e.which,sm=Sd.moment,wh=$(window).innerHeight();
+                	var	so,to,too,ek=e.which,sm=_this.moment,wh=$(window).innerHeight();
                	
                 	sm.speedX = (ek in {37:0,100:0}?2:(ek in {39:0,102:0}?-2:0 ) );
                 	sm.speedY = (ek in {38:0,104:0}?1:(ek in {40:0,98:0}?-1:(ek in {33:0,105:0}? (so=Math.sqrt(Math.sqrt(wh)))*Math.sqrt(so/3)-4 :(ek in {34:0,99:0}? -(so=Math.sqrt(Math.sqrt(wh)))*Math.sqrt(so/3)+4 :(ek in {35:0,97:0}?-88:(ek in {36:0,103:0}?88:0) ) ) ) ) );
@@ -193,21 +211,30 @@ $(function () {
 									&& (so=to.offset()).top <=(ek=wh-to.innerHeight()) 
 									&& (so.top >=0 || ek<0 && so.top >=ek) )
 								)) 
-						(to=to.slice(1)).length || (to=too=too.children());
+							(to=to.slice(1)).length || (to=too=too.children());
 	                	S.too = to.eq(0);
                 	};                	
                 	             	               	
-					Sd.onReleaseEasing();
+					_this.onReleaseEasing();
 					e.preventDefault(); e.stopPropagation();
+                },  
+                onTouch: function (e) {
+                    S.to = $((this === e.target) ? this : e.target);
+                    var c=(e.type=='touchstart')?e.originalEvent.touches[0]:e;
+                    S.holdPos = {'x':e.screenX, 'y':e.screenY};
+                    do {
+                    	
+                    } while ((S.to.data('overflow') == 'no-dragon' || S.to[0].tagName.toLowerCase() == 'a')
+							&& S[0] != S.to[0]
+							&& (S.to = S.to.parent()));
                 }
             };
 					
-            S.on({'DragOn.toggle':Sd.toggle,'DragOn.remove':function(){Sd.on||Sd.toggle();Bo=null;S.off('DragOn.toggle DragOn.remove')}});
-            Sd.toggle();
+            S.on({'DragOn.toggle':_this.toggle,'DragOn.remove':function(){_this.on||_this.toggle();Bo=null;S.off('DragOn.toggle DragOn.remove')}});
+            _this.toggle();
 			
-            (("Info" in window) && Info||console).log('DragOn fly...');
-            return Sd;
-
+            (("Info" in window) && Info || console).log('DragOn fly...');
+            return _this;
         }
     });
 

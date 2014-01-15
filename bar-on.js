736d2ea9598e29d3d47424ee138de7a80077f6fc
PretorDH <pretordh@deparadox.com>
2014-01-15 21:23:55 +0200

    Bar-on: new version 0.4.4.
    ! Fix: modification of css "overflow-..." propertion
    ! Fix: bugs with undefined S.to in DrawScrool();
    _ Opt: code optimization

--- bar-on.js
+++ bar-on.js
@@ -1,5 +1,5 @@
 ﻿/**
- * jQuery.Bar-On v0.4.2
+ * jQuery.Bar-On v0.4.4
  * @author Dark Heart aka PretorDH
  * @site dragon.deparadox.com/#baron
  * MIT license
@@ -37,7 +37,7 @@ $.extend({
 			        Bo.on=!Bo.on;
             	},
             	drawScroll : function (e) {
-            		if (!S.to[0]) return;
+            		if (!S||!S.to||!S.to[0]) return;
             		var a,t,ox=true,oy=true,ih,sh,iw,sw;
             		rB.css({top:0,bottom:0,right:0});
             		bB.css({bottom:0,left:0,right:0});
@@ -73,8 +73,10 @@ $.extend({
             	},
             	onScroll : function (e,x,y) {
             		var et;
-            		S.to=(S.too.parents().filter(et=$(e.target)).length)?S.too:(S.too=et);
-            		Bo.drawScroll(e);
+            		if (S.too) {
+	            		S.to=(S.too.parents().filter(et=$(e.target)).length)?S.too:(S.too=et);
+	            		Bo.drawScroll(e);
+            		}
             	},
             	onHover : function (e) {
             		if ((S.to = $(e.target))[0]==(S.too||[''])[0]) return;
@@ -85,10 +87,12 @@ $.extend({
 		            $('*',S).add(S).each(e);
             	},
             	clearScroll : function (j,s) {
-            		var i,dx=(s=$(s)).css('overflow-x'), dxd=s.data('overflow-x'), ps = s.css('position'),
-            			dy=       s.css('overflow-y'), dyd=s.data('overflow-y'); i=true;
-	           		if (dx in Bo.doList && dxd!='no-baron') s.data('overflow-x',dx).css({'overflow-x':'hidden','position':(ps=='static')?'relative':((i=false),ps)});
-	           		if (dy in Bo.doList && dyd!='no-baron') s.data('overflow-y',dy).css({'overflow-y':'hidden','position':(ps=='static')?'relative':((i=false),ps)});
+            		var i=true,
+            			dx=(s=$(s)).css('overflow-x'), dxd=s.data('overflow-x'), ps = s.css('position'),
+            			dy=       s.css('overflow-y'), dyd=s.data('overflow-y'), pso = (ps=='static')?{position:'relative'}:{} ;            		
+	           		if (dx in Bo.doList && dxd!='no-baron') (s.data('overflow-x',dx),(pso['overflow-x']='hidden'),i=false);
+	           		if (dy in Bo.doList && dyd!='no-baron') (s.data('overflow-y',dy),(pso['overflow-y']='hidden'),i=false);
+	           		s.css(pso);
 	           		if (j==null) return i;
             	},
             	restoreScroll : function (j,s) {
